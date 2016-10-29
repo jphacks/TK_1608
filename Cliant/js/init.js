ec2510b5d4ad3bdc41eecf1321b184f645602636
Kyome22 <kyomesuke@gmail.com>
2016-10-29 11:09:00 +0900

    animationできました。

--- Cliant/js/init.js
+++ Cliant/js/init.js
@@ -1,5 +1,18 @@
+var count = 0;
+
+function loadFirst() {
+    $.fn.fullpage.silentMoveTo(0, 1);
+    setInterval("timer()", 3000);
+}
+
+function timer() {
+    count = (count + 1) % 3;
+    $.fn.fullpage.moveTo(0, count);
+}
+
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        controlArrows : false
+        controlArrows : false,
+        // anchors: ['fiveBomber', 'chat', 'shiritori']
     });
 });

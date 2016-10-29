174c324d35325da0bab934f748281d0d56b974db
Kyome22 <kyomesuke@gmail.com>
2016-10-29 15:37:39 +0900

    配置例

--- Cliant/js/init.js
+++ Cliant/js/init.js
@@ -1,18 +1,24 @@
-var count = 0;
+var count = 60;
 
 function loadFirst() {
-    $.fn.fullpage.silentMoveTo(0, 1);
-    setInterval("timer()", 3000);
+    $.fn.fullpage.silentMoveTo(0, 0);
+    setInterval("timer()", 1000);
 }
 
 function timer() {
-    count = (count + 1) % 3;
-    $.fn.fullpage.moveTo(0, count);
+    count = count - 1;
+    document.getElementById("remainTime").textContent = count;
+    if(count < 0){
+        count = 60;
+    }
+    // count = (count + 1) % 3;
+    // $.fn.fullpage.moveTo(0, count);
 }
 
 $(document).ready(function() {
     $('#fullpage').fullpage({
         controlArrows : false,
+        verticalCentered : false
         // anchors: ['fiveBomber', 'chat', 'shiritori']
     });
 });
